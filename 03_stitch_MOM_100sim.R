e505e1bb1352f4b1986297ab98c6c8467f55765b
Quang C. Huynh <quang@bluematterscience.com>
2022-03-31 17:16:49 -0700

    Figures with no process error in projections

---
+++ 03_stitch_MOM_100sim.R
@@ -0,0 +1,35 @@
+
+
+library(MSEtool)
+library(dplyr)
+
+source("MOM_fn.R")
+
+byc <- c("BSH", "SMA", "WHM", "BUM")
+targ <- c("BET", "SWO")
+
+#multiHist_byc <- lapply(paste0('MOM/multiHist_', byc, '_100sim.rds'), readRDS)
+#multiHist_targ <- lapply(paste0('MOM/multiHist_', targ, '_100sim.rds'), readRDS)
+
+MOM_byc <- lapply(paste0('MOM/MOM_', byc, '_100sim.rds'), readRDS)
+MOM_targ <- lapply(paste0('MOM/MOM_', targ, '_100sim.rds'), readRDS)
+
+LL_byc <- list(1:9, c(1:12)[-c(8, 10, 12)], 2, 2) %>% structure(names = byc)
+LL_targ <- list(10:18, 1:11) %>% structure(names = targ)
+
+M2_byc <- Map(function(x, y) aggregate_fleet(x = x, LL = y), x = MOM_byc, y = LL_byc)
+M2_targ <- Map(function(x, y) aggregate_fleet(x = x, LL = y), x = MOM_targ, y = LL_targ)
+
+MOM <- local({
+  args <- c(M2_targ, M2_byc) %>% structure(names = c(targ, byc))
+  do.call("MOM_stitch", args)
+})
+saveRDS(MOM, file = "MOM/MOM_stitch_100sim.rds")
+#MOM <- readRDS(file = "MOM/MOM_stitch_100sim.rds")
+
+setup(8)
+multiHist <- multiMSE(MOM, Hist = TRUE, checkMPs = FALSE, parallel = TRUE)
+sfStop()
+saveRDS(multiHist, file = "MOM/multiHist_stitch_100sim.rds")
+#multiHist <- readRDS(file = "MOM/multiHist_stitch_100sim.rds")
+
