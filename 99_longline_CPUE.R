79bf0a77072bc1f3e90bf6d39be4cd3e83a4553c
Quang C. Huynh <quang@bluematterscience.com>
2022-12-09 10:43:57 -0800

    Preliminary T2CE analysis

--- 99_longline_CPUE.R
+++ 99_longline_CPUE.R
@@ -7,8 +7,15 @@ dir <- "G:/Shared drives/BM shared/1. Projects/EcoTest/Databases"
 LLCE <- read.csv(file.path(dir, "t2ce_LL.csv"))
 
 # Calculate longline CPUE as catch/Eff1 - aggregated at trip level?
-LL <- LLCE %>%
+LLN <- LLCE %>%
   filter(CatchUnit == "nr", Eff1Type == "NO.HOOKS") %>%
+  mutate(Lat = ifelse(QuadID == 1 | QuadID == 4, Lat, -1 * Lat),
+         Lon = ifelse(QuadID == 1 | QuadID == 2, Lon, -1 * Lon),
+         BET = BET/Eff1, SWO = SWO/Eff1, WHM = WHM/Eff1, BUM = BUM/Eff1, SMA = SMA/Eff1, BSH = BSH/Eff1) %>%
+  select(YearC, Lat, Lon, BET, SWO, BUM, WHM, BSH, SMA, FlagName)
+
+LLW <- LLCE %>%
+  filter(CatchUnit == "kg", Eff1Type == "NO.HOOKS") %>%
   mutate(Lat = ifelse(QuadID == 1 | QuadID == 4, Lat, -1 * Lat),
          Lon = ifelse(QuadID == 1 | QuadID == 2, Lon, -1 * Lon),
          BET = BET/Eff1, SWO = SWO/Eff1, WHM = WHM/Eff1, BUM = BUM/Eff1, SMA = SMA/Eff1, BSH = BSH/Eff1) %>%
@@ -17,27 +24,104 @@ LL <- LLCE %>%
 # Species combinations
 sp <- c("BET", "SWO", "BSH", "SMA", "WHM", "BUM")
 
-# Calculate spatial correlation in CPUE
-get_corr <- local({
-  tab <- lapply(1:length(sp), function(x) expand.grid(sp[x], sp[-c(1:x)], stringsAsFactors = FALSE)) %>% bind_rows()
+# Calculate joint catches
+get_jc <- function(LL, fname) {
+  
+  tab <- lapply(1:length(sp), function(x) expand.grid(sp[x], sp[x:length(sp)], stringsAsFactors = FALSE)) %>% bind_rows()
   
   lapply(1:nrow(tab), function(x) {
     s1 <- as.symbol(tab$Var1[x])
     s2 <- as.symbol(tab$Var2[x])
     
     ##### Aggregate by year and spatial area - perhaps too fine for calculating correlation among CPUE
-    corr <- group_by(LL, YearC, Lat, Lon) %>% 
-      summarise(corr = cor(!!s1, !!s2)) %>%
-      filter(!is.na(corr)) %>%
-      mutate(S1 = tab$Var1[x], S2 = tab$Var2[x])
+    if(s1 == s2) {
+      n_records <- LL %>% filter(FlagName == fname) %>% group_by(YearC) %>% summarise(n = n())
+      pos <- LL %>% filter(!!s1 > 0, FlagName == fname) %>%
+        group_by(YearC) %>%
+        summarise(npos = n())
+    } else {
+      n_records <- LL %>% filter(!!s1 > 0 || !!s2 > 0, FlagName == fname) %>% group_by(YearC) %>% summarise(n = n())
+      pos <- LL %>% filter(!!s1 > 0, !!s2 > 0, FlagName == fname) %>%
+        group_by(YearC) %>%
+        summarise(npos = n())
+    }
     
-    corr
+    left_join(pos, n_records, by = "YearC") %>% mutate(S1 = tab$Var1[x], S2 = tab$Var2[x], p_pos = npos/n, FlagName = fname)
+      
+  }) %>% 
+    bind_rows() %>% 
+    mutate(S1 = factor(S1, levels = sp), S2 = factor(S2, levels = sp))
+  
+}
+
+# Calculate spatial correlation in CPUE from non zero catch
+get_corr <- function(LL, fname) {
+  tab <- lapply(1:length(sp), function(x) expand.grid(sp[x], sp[x:length(sp)], stringsAsFactors = FALSE)) %>% bind_rows()
+  
+  lapply(1:nrow(tab), function(x) {
+    s1 <- as.symbol(tab$Var1[x])
+    s2 <- as.symbol(tab$Var2[x])
+    
+    ##### Aggregate by year and spatial area - perhaps too fine for calculating correlation among CPUE
+    LL %>% filter(!!s1 > 0, !!s2 > 0, FlagName == fname) %>%
+      group_by(YearC) %>%
+      summarise(corr = cor(!!s1, !!s2)) %>%
+      mutate(S1 = tab$Var1[x], S2 = tab$Var2[x], FlagName = fname)
+    
     
   }) %>% 
     bind_rows() %>% 
     mutate(S1 = factor(S1, levels = sp), S2 = factor(S2, levels = sp))
   
-})
+}
+
+# Japan
+JPN <- get_jc(LLN, "Japan")
+JPN_corr <- get_corr(LLN, "Japan")
+
+# TPE
+TPE <- get_jc(LLW, "Chinese Taipei")
+TPE_corr <- get_corr(LLW, "Chinese Taipei")
+
+# USA
+USA <- get_jc(LLN, "U.S.A.")
+USA_corr <- get_corr(LLN, "U.S.A.")
+
+ESP <- get_jc(LLN, "EU.España")
+ESP_corr <- get_corr(LLN, "EU.España")
+
+BRA <- get_jc(LLW, "Brazil")
+BRA_corr <- get_corr(LLW, "Brazil")
+
+pos <- rbind(JPN, TPE, USA, ESP, BRA)
+  
+g <- ggplot(pos, aes(YearC, p_pos, colour = FlagName)) + 
+  facet_grid(vars(S1), vars(S2)) + 
+  geom_line() + 
+  #geom_point() + 
+  theme_bw() +
+  theme(panel.spacing = unit(0, "in"),
+        axis.text.x = element_text(angle = 45, vjust = 0.5)) + 
+  labs(x = "Year", y = "Proportion positive catches of both species", colour = "Flag")
+ggsave("Figures/LLCE/LL_spatial_pos.png", g, height = 6, width = 10)
+
+f_corr <- rbind(JPN_corr, 
+                TPE_corr, 
+                USA_corr, 
+                ESP_corr, 
+                BRA_corr)
+
+g <- ggplot(f_corr, aes(YearC, corr, colour = FlagName)) + 
+  facet_grid(vars(S1), vars(S2)) + 
+  geom_hline(yintercept = 0, linetype = 2) + 
+  geom_line() + 
+  #geom_point() + 
+  theme_bw() +
+  theme(panel.spacing = unit(0, "in"),
+        axis.text.x = element_text(angle = 45, vjust = 0.5)) + 
+  labs(x = "Year", y = "CPUE correlation", colour = "Flag")
+ggsave("Figures/LLCE/LL_spatial_corrs.png", g, height = 6, width = 10)
+
 
 
 ### Scroll through annual longline CPUE correlations
@@ -51,7 +135,7 @@ ggplot(get_corr, aes(Lon, Lat, colour = corr)) + facet_grid(vars(S1), vars(S2)) 
 
 ### Save to disk year-specific map with coastline
 year_to_plot <- 2009
-coast <- rnaturalearth::ne_coastline(scale = 10, returnclass = "sf")
+coast <- rnaturalearth::ne_coastline(scale = 110, returnclass = "sf")
 
 g <- get_corr %>% filter(YearC == year_to_plot) %>%
   ggplot(aes(Lon, Lat, colour = corr)) + facet_grid(vars(S1), vars(S2)) + theme_bw() + 
