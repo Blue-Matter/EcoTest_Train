476b5e056fa72bf35c72454c4b7fa8c05dd5c2a5
tcarruth <tomsinbox@gmail.com>
2024-04-19 10:44:06 -0700

    some updates

--- 11_b_train_indicator.R
+++ 11_b_train_indicator.R
@@ -22,19 +22,20 @@ source('99_neural_net.R')
 
 # --- makes datasets for AI training ------------------------------------------------------
 
-allout = readRDS("Indicator/Processed_data_3.rds")
-TD = makerawdata(allout, sno=2, isBrel=F, inc_spat = F, inc_Irel = F)
-TD[,1] = log(TD[,1])
+#  1   2   3   4   5   6
+# BET SWO BSH SMA WHM BUM
+
+allout = readRDS("Indicator/Processed_data_4.rds")
+TD = makerawdata(allout, sno=1, isBrel=F, inc_spat = T, inc_Irel = T)
 
 nr<-nrow(TD)
 nc<-ncol(TD)
 ind<-(1:nr)%in%sample(1:nr,floor(nr*0.05),replace=FALSE)
 
-
-
 train<-TD[!ind,2:nc]
 mu = apply(train,2,mean)
 sd = apply(train,2,sd)
+
 train = scale(train,center=mu,scale=sd)
 train_target<-TD[!ind,1]
 testy<-TD[ind,2:nc]
@@ -44,8 +45,8 @@ testy_target<-TD[ind,1]
 model = keras_model_sequential()
 
 model %>%
-  layer_dense(units = 4, activation = "relu", input_shape = c(nc-1)) %>%
-  layer_dense(units = 2, activation = "relu") %>%
+  layer_dense(units = 8, activation = "relu", input_shape = c(nc-1)) %>%
+  layer_dense(units = 4, activation = "relu") %>%
   layer_dense(units = 1)
   
   
@@ -56,19 +57,18 @@ model %>%
     metrics = "MAE"
   )
  
+nepoch = 20 
 
 history <- model %>% fit(train, train_target,
-  epochs = 30,
+  epochs = nepoch,
   validation_split = 0.2,
-  verbose = 2#,
-  #callbacks = list(print_dot_callback)
+  verbose = 2
 )
 
-#plot(history)
 
 pred <- model %>% predict(testy)
-
 plot(exp(testy_target),exp(pred[ , 1]),xlab="SSB/SSBMSY (simulated)",ylab="SSB/SSBMSY (pred)"); lines(c(0,1E10),c(0,1E10),col='#ff000050',lwd=2)
+legend('topleft',legend = paste("MAE_val:",round(history$metrics$val_MAE[nepoch],4)),bty="n")
 
 
 power_tab(sim = testy_target, pred = pred)
@@ -81,7 +81,7 @@ power_tab(sim, pred, lev = 0.5){
   
 }
 
-  
+plot(history)  
 saveRDS(history,'Ehistory_26_26.rda')
   
   
--- 99_neural_net.R
+++ 99_neural_net.R
@@ -1,7 +1,7 @@
 
 
 
-cleandat = function(dat,resprange=c(0.05,2)){
+cleandat = function(dat,resprange=c(-3.689,0.693)){
   goodrow = function(x)(sum(is.na(x)| x==0 | x==1 | x == 'Inf' | is.null(x)| x=="NULL")==0)&x[1]>resprange[1] & x[1]<resprange[2]
   dat[apply(dat,1,goodrow),]
 }
@@ -14,21 +14,15 @@ reformat = function(x){
 }
 
 makerawdata = function(allout, sno=1, isBrel = F, clean = T, 
-                       inc_spat = T, inc_Irel = T, inc_L50_Linf = F){
+                       inc_spat = T, inc_Irel = T){
   
   cdat = as.data.frame(rbindlist(allout))
-  
-  
-  #for(i in 1:ncol(cdat)) names(cdat)[i] = gsub("-","_",names(cdat)[i])
   dnames = names(cdat)
- 
   Brelcols = grepl("Brel",dnames)
   ns = sum(Brelcols)
-  
   keepcol = grep(paste0("Brel_",sno),dnames)
-  
-  Res = cdat[,keepcol]
-  
+  Res = log(cdat[,keepcol])
+ 
   # level relative to mean level of other stocks
   if(isBrel){Res = ((ns-1) * Res) /(apply(cdat[,Brelcols],1,sum)-Res)}
   
@@ -36,12 +30,6 @@ makerawdata = function(allout, sno=1, isBrel = F, clean = T,
   
   if(!inc_spat) dat = dat[,!grepl('spat',names(dat))]
   if(!inc_Irel) dat = dat[,!grepl('I_rel',names(dat))]
-  if(!inc_L50_Linf){
-    cond = grepl("L50",names(dat)) & !grepl("ML_L50",names(dat))
-    dat= dat[,!cond]
-    cond = grepl("Linf",names(dat)) & !grepl("ML_Linf",names(dat))
-    dat= dat[,!cond]
-  }
   
   # log ratios
   tolog = grepl("C_rel", names(dat)) | grepl("CR_mu",names(dat)) | grepl("FM_cur", names(dat)) | grepl("FM_rel", names(dat)) |
@@ -49,8 +37,7 @@ makerawdata = function(allout, sno=1, isBrel = F, clean = T,
     grepl("MV_rel",names(dat))| grepl("ML_Linf" ,names(dat)) |   grepl("ML_L50" , names(dat)) |
     grepl("CR_rel",names(dat))
   
-  dat[,cond] = log(dat[,tolog])
-  
+  dat[,tolog] = log(dat[,tolog])
   if(clean)dat=cleandat(dat)
   dat
   
