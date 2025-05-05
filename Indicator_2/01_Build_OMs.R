1d8f9ca3ec3836d367025fb7b823a59791b4b857
tcarruth <tomsinbox@gmail.com>
2025-05-05 08:27:06 -0700

    Update 01_Build_OMs.R

--- Indicator_2/01_Build_OMs.R
+++ Indicator_2/01_Build_OMs.R
@@ -10,10 +10,17 @@ setwd("C:/GitHub/EcoTest")
 source.all("Source")
 
 largedir = "C:/Users/Admin/Dropbox/temp/Ecotest/Ind2"
-setup(cpus=20)
-Ind2Export()
+setup(cpus=48)
+#Ind2Export()
 
-system.time({  sfSapply(1:20, runbatch2, nsim = 4)  })
+for(x in 1:100) runbatch2(x, nsim=48)
+
+
+for(x in 101:500) runbatch2(x, nsim=48)
+
+for(x in 501:800) runbatch2(x, nsim=48)
+
+#system.time({  sfSapply(1:20, runbatch2, nsim = 4)  })
 
 
 # can probably do this with depletion x VB0 x Find[lastyr] or there abouts (with some error maybe)
