a8c1854686703538aac2961f5563b9bb8c7cd726
tcarruth <tomsinbox@gmail.com>
2025-09-12 10:33:09 -0700

    Indicator 3 retry

--- Source/i3_neural_net.R
+++ Source/i3_neural_net.R
@@ -7,19 +7,13 @@ dolog_3=function(dat){
                              "ML_L50","CR_rel","maxa","K","M_K","L50_Linf","L5_L50", 
                              "LFS_L50","Csd","CF","Isd"))
 }
-
-fixnames3 = function(allout3){
-  weirdnames = paste0(c("Brel","K","M_K","maxa","L50_Linf")
-  
-}
-                          
+                      
                          
 makerawdata_3 = function(allout3, sno=1, isBrel = F, clean = T, 
                        inc_Irel = T, inc_I = T, inc_CR = T, inc_CAL = T, inc_CAA = T,
                        stock_in = NA, fleet_in = NA, Bmin = 0.1){
   # sno=1; isBrel = F; clean = T;  inc_Irel = T; inc_I = T;  inc_CR = T; stock_in = NA; inc_CAL = T; inc_CAA = T
   cdat = as.data.frame(rbindlist(allout3))
-  if("Brel_s1_12"%in%names(cdat))allout3 = fixnames3(allout3)
   dnames = names(cdat)
   Brelcols = grepl("Brel",dnames)
   stocks = sapply(dnames[Brelcols],function(x)strsplit(x,"_")[[1]][2])
