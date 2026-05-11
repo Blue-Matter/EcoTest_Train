1489a37c9e2055822b7f2f9a87f230558bff05c7
tcarruth <tomsinbox@gmail.com>
2026-05-11 13:54:39 -0700

    Update 02_make_large_PPD_dataset_INDICATOR_3.R

--- Indicator_3/02_make_large_PPD_dataset_INDICATOR_3.R
+++ Indicator_3/02_make_large_PPD_dataset_INDICATOR_3.R
@@ -15,11 +15,11 @@ source.all("Source")
 
 # prediction is 60 minutes for workstation (48 x 1.0)  
 
-# First 200 runs (9600 sims)
+# 45600 sims
 
 MSEdir = "C:/Users/tcar_/Dropbox/temp/Ecotest/Ind3/MMSE"
 system.time({allout = process_sim_data_3(MSEdir, parallel=T, cores = parallel::detectCores())})
-saveRDS(allout,"Indicator_3/allout_1_200.rds")
+saveRDS(allout,"Indicator_3/New_allout.rds")
 
 
 
